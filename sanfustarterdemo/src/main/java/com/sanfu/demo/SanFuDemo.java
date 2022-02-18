fd134d8c8e35c8669c9c8f2b6c29ef3b146fe314
Kevincfb <1071679346@qq.com>
2022-02-18 11:39:32 +0800

    首次提交

---
+++ sanfustarterdemo/src/main/java/com/sanfu/demo/SanFuDemo.java
@@ -0,0 +1,34 @@
+package com.sanfu.demo;
+
+/**
+ * SanFuDemo
+ */
+public class SanFuDemo {
+    private String name;
+    private String url;
+    private String desc;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
